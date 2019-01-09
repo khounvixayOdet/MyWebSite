28d6c5eff2fc1350589bde4f6541bd0bf6dbb1dd
vilakone1 <46478518+vilakone1@users.noreply.github.com>
2019-01-09 14:03:19 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,79 @@
-<?php
-echo "vilakon"
-echo "dai hoc ha tinh"
-echo "k8cntt"
-echo"  xin chao"
-  echo" so cute "
-  echo"bounthavy sengmany"
-?>
+<?php include("stye.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Bootstrap 101 Template</title>
+
+    
+    
+
+    
+  </head>
+  <body>
+    <?php include("menu.php"); ?>
+    <div class="container">
+    <?php include("slide.php"); ?>
+    <div class="row">
+  <div class="col-md-3">
+  	<div class="list-group">
+  <a href="#" class="list-group-item active">
+    Cras justo odio
+  </a>
+  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+  <a href="#" class="list-group-item">Morbi leo risus</a>
+  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+  <a href="#" class="list-group-item">Vestibulum at eros</a>
+</div>
+  </div>
+  <div class="col-md-9"><div class="panel panel-primary">
+      <div class="panel-heading">Panel with panel-primary class</div>
+      <div class="panel-body">
+      	<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="img/anhsanpham.jpeg" alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="img/anhsanpham2.jpeg" alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="img/anhsanpham3.jpeg" alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+    
+    </div>
+</div>
+    
+  
+</div>
+
+    
+    
+  </body>
+</html>
