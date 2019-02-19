a06a398f346e2026de97a8de3ee7fc72f4345a57
khounvixayOdet <46478555+khounvixayOdet@users.noreply.github.com>
2019-02-19 14:40:56 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,79 +1,62 @@
-<?php include("stye.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>Bootstrap 101 Template</title>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<title>Odet store</title>
+</head>
 
-    
-    
-
-    
-  </head>
-  <body>
-    <?php include("menu.php"); ?>
-    <div class="container">
-    <?php include("slide.php"); ?>
-    <div class="row">
-  <div class="col-md-3">
-  	<div class="list-group">
-  <a href="#" class="list-group-item active">
-    Cras justo odio
-  </a>
-  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-  <a href="#" class="list-group-item">Morbi leo risus</a>
-  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-  <a href="#" class="list-group-item">Vestibulum at eros</a>
-</div>
-  </div>
-  <div class="col-md-9"><div class="panel panel-primary">
-      <div class="panel-heading">Panel with panel-primary class</div>
-      <div class="panel-body">
-      	<div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="img/anhsanpham.jpeg" alt="...">
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>...</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-      </div>
+<body>
+    <div class="wraper">
+        <div class="header">
+        	<img src="Pic.jpg" width="100%" height="120px"/>
+        </div>
+        <br>
+        <div class="menu" style="border-radius: 5px;">
+        <h4>
+        	<ul>
+                <li><a href="#">Lien he</li>
+                <li><a href="#">Huong dan</li>
+                <li><a href="#">San pham</li>
+                <li><a href="#">Home</li>
+            </ul>
+		</h4>
+        </div>
+        <div class="content">
+        	<div class="left"><a href="#"></a>
+        		<h2 style=" color:#FFFFFF; background:#337ab7; padding: 10px; border-radius: 5px;">Categortes</h2>
+        		<div class="danhsachsanpham">
+        			<ul>
+        				<li><a href="#">Iphone</a></li>
+        				<li><a href="#">Teblet</a></li>
+        				<li><a href="#">Smartphone</a></li>
+        				<li><a href="#">Camera</a></li>
+        			</ul>
+        			<h2 style="color:#FFFFFF; background:#337ab7;padding: 10px; border-radius: 5px;">Brands</h2>
+        			<ul>
+        				<li><a href="#">Apple</a></li>
+        				<li><a href="#">SONY</a></li>
+        				<li><a href="#">SAMSUNG</a></li>
+        				<li><a href="#">MSI</a></li>
+					</ul>
+				</div>
+        	</div>
+        	<div class="right">
+        		<p style="text-align:  color: aliceblue; background:#d9edf7; padding: 10px; border-radius: 5px;">Products</p>
+        		<div class="sanphamall">
+        			<ul>
+        				<li><a href="#">
+        					<img src="anh/msi gf63.png" width="100px" height="100px"/>
+        					<p >MSI</p>
+        					<del>Gia Thuong:5.000.000d</del>
+        					<p style="color: #FF161A" >Gia sp:2.000.000d</p>
+        					<p >chi tiet</p>
+        				</a></li>
+        			</ul>
+        		</div>
+        	</div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer" style="text-align: center;">khounvixay Odet</div>
     </div>
-  </div>
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="img/anhsanpham2.jpeg" alt="...">
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>...</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="img/anhsanpham3.jpeg" alt="...">
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>...</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-      </div>
-    </div>
-  </div>
-</div>
-      </div>
-    </div>
-    
-    </div>
-</div>
-    
-  
-</div>
-
-    
-    
-  </body>
+</body>
 </html>
